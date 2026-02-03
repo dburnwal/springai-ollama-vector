2282faf2dfcbf0f84ce23f95aa25bc4258cf9ac3
Deepak Burnwal <burnwal.deepak@gmail.com>
2026-02-03 12:44:16 +0530

    Initial commit

---
+++ src/main/java/com/dburnwal/springai/service/OllamaService.java
@@ -0,0 +1,25 @@
+package com.dburnwal.springai.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OllamaService {
+	
+	private ChatClient chatClient;
+	
+	public OllamaService(ChatClient.Builder builder) {
+		chatClient = builder.defaultAdvisors(new MessageChatMemoryAdvisor
+				(new InMemoryChatMemory())).build();
+	}
+	
+	public ChatResponse generateAnswer(String question) {
+		return chatClient.prompt(question).call().chatResponse();
+	}
+
+	
+
+}
